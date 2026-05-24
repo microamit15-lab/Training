3208e75deacbca80852a641f879cb6abe8cc0d24
microamit15-lab <micro.amit15@gmail.com>
2026-05-24 10:42:44 +0530

    Update StringPermutation.java

--- CoreJava/src/basics/logic/StringPermutation.java
+++ CoreJava/src/basics/logic/StringPermutation.java
@@ -1,27 +1,38 @@
-package basics.logic;
+package main;
 
-public class StringPermutation {
-	
-	public static void permutation(String str, String avg) {
-		
-		
-		if(str.length()==0) {
-			System.out.println(avg);
-			return;
-		}
-		
-		for(int i =0 ; i< str.length() ; i++ ) {
-			char ch = str.charAt(i);
-			String remaining = str.substring(i+1) + str.substring(0,i);
-			permutation(remaining, avg+ch);
-		}
-		
+public class Permutation {
+
+
+
+	    // Function to generate permutations
+	    public static void permute(String str, String ans) {
+
+	        // Base case
+	        if (str.length() == 0) {
+	            System.out.println("ans : "+ ans);
+	            return;
+	        }
+
+	        // Recursive case
+	        for (int i = 0; i < str.length(); i++) {
+
+	            // Pick character
+	            char ch = str.charAt(i);
+	            System.out.println("iteration : " + i + "  CH : " + ch);
+	            // Remaining string after removing picked character
+	            String remaining = str.substring(0, i) + str.substring(i + 1);
+	            System.out.println("i : " +i );
+	            System.out.println( "str.substring(0, i) : "+ str.substring(0, i));
+	            System.out.println("str.substring(i + 1) :  " +str.substring(i + 1));
+	            System.out.println("remaining : " + remaining + " ANS : " + ans+ch);
+
+	            // Recursive call
+	            permute(remaining, ans + ch);
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	        String input = "ABC";
+	        permute(input, "");
+	    }
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		permutation("abc", "");
-
-	}
-
-}
